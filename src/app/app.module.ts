93cdf863606aa12e9e4702dd5f6fde4de4f1669d
MassimilianoDallaMura <dalla.mura.91@gmail.com>
2024-03-19 16:56:17 +0100

    esercizio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,8 @@ import { AppComponent } from './app.component';
 import { Component1Component } from './components/component1/component1.component';
 import { Component2Component } from './components/component2/component2.component';
 import { Component3Component } from './components/component3/component3.component';
-import { Component4Component } from './component4/component4.component';
+import { Component4Component } from './components/component4/component4.component';
+
 
 @NgModule({
   declarations: [
@@ -13,7 +14,7 @@ import { Component4Component } from './component4/component4.component';
     Component1Component,
     Component2Component,
     Component3Component,
-    Component4Component
+    Component4Component,
   ],
   imports: [
     BrowserModule
